06c660f7d1537a8a8a9faf5a6b943e603e645099
Lunarestia <jsaccoia24@gmail.com>
2021-08-06 18:39:23 -0400

    commit on module6

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-const host = process.env.DB_HOST || '127.0.0.1'
-const dbURI = `mongodb://${host}/travlr`;
 const readLine = require('readLine');
 
-//avoid 'current Server Discovery and Monitoring engine is deprecated'
-mongoose.set('useUnifiedTopology', true);
+let dbURL = 'mongodb://127.0.0.1/travlr';
+if (process.env.NODE_ENV === 'production') {
+    dbURL = process.env.DB_HOST || process.env.MONGODB_URI;
+}
 
 const connect = () => {
-    setTimeout (() => mongoose.connect(dbURI, {
+    setTimeout (() => mongoose.connect(dbURL, {
         useNewUrlParser: true,
-        useCreateIndex: true
+        useCreateIndex: true,
+        useUnifiedTopology: true
     }), 1000);
 }
 
